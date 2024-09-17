537a337239b60ab9aba76083ab85caef548c931e
Ibrahim Mondal <ibrahimmondal750@gmail.com>
2024-09-17 20:17:54 +0530

    all done

---
+++ text-8/loop.js
@@ -0,0 +1,18 @@
+let car =["BMW", "TATA", "Volvo", "Toyota", "Tesla"];
+
+car.push("ibo")
+
+// for loop
+
+for (let i = 0; i < car.length; i++){
+   console.log(car[i]);
+   
+}
+
+// for in loop 
+
+let man = {
+    name: "ibrahim";
+    age: 21;
+    cuntry: "india";
+}
