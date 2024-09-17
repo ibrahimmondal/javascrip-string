111028d3c58f649806667b9b06dfa1802946e0c4
Ibrahim Mondal <ibrahimmondal750@gmail.com>
2024-09-17 21:27:57 +0530

    Loop

--- text-8/loop.js
+++ text-8/loop.js
@@ -1,18 +1,23 @@
-let car =["BMW", "TATA", "Volvo", "Toyota", "Tesla"];
+// let car =["BMW", "TATA", "Volvo", "Toyota", "Tesla"];
 
-car.push("ibo")
+// car.push("ibo");
 
-// for loop
+// // for loop
 
-for (let i = 0; i < car.length; i++){
-   console.log(car[i]);
+// for (let i = 0; i < car.length; i++){
+//    console.log(car[i]);
    
-}
+// }
 
 // for in loop 
 
 let man = {
-    name: "ibrahim";
-    age: 21;
-    cuntry: "india";
+    name: "ibrahim",
+    age: 21,
+    cuntry: "india", 
+}
+for (let i in man){
+    let capletter = i.charAt(0).toUpperCase() + i.slice(1);
+    console.log(capletter + ": " + man[i]);
+    
 }
